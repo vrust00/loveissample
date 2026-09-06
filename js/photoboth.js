f84b5295cd89750d11193533b72c28577b45c5f4
Ivan Tumanov <vrust00@yandex.ru>
2026-09-06 23:08:36 +0400

    -

--- js/photoboth.js
+++
@@ -1,8 +0,0 @@
-btn = document.getElementById('photoboth-btn')
-photoline = document.getElementById('photoline')
-
-btn.addEventListener('click', () => {
-   photoline.classList.toggle('hide');
-   console.log(document.getElementById('photoline'));
-   console.log(document.getElementById('photoboth-btn'));
-})
